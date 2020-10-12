33fa3f3d9e1529c1af29d26c29a7ab0b6e2580fd
Anuj Rajput <anujrajput04@gmail.com>
2020-10-12 19:10:38 +0530

    Setting up the view

---
+++ Projects/RabbleWabble/RabbleWabble/Controllers/SelectQuestionGroupViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SelectQuestionGroupViewController.swift
+//  RabbleWabble
+//
+//  Created by Anuj Rajput on 12/10/20.
+//
+
+import UIKit
+
+public class SelectQuestionGroupViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet internal var tableView: UITableView! {
+        didSet {
+            tableView.tableFooterView = UIView()
+        }
+    }
+    
+    // MARK: - Properties
+    public let questionGroups = QuestionGroup.allGroups()
+    private var selectQuestionGroup: QuestionGroup!
+    
+}
+
+// MARK: - UITableViewDataSource
+extension SelectQuestionGroupViewController: UITableViewDataSource {
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questionGroups.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionGroupCell") as! QuestionGroupCell
+        let questionGroup = questionGroups[indexPath.row]
+        cell.titleLabel.text = questionGroup.title
+        return cell
+    }
+}
---
+++ Projects/RabbleWabble/RabbleWabble/Views/QuestionGroupCell.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionGroupCell.swift
+//  RabbleWabble
+//
+//  Created by Anuj Rajput on 12/10/20.
+//
+
+import UIKit
+
+public class QuestionGroupCell: UITableViewCell {
+    @IBOutlet public var titleLabel: UILabel!
+    @IBOutlet public var percentageLabel: UILabel!
+}
