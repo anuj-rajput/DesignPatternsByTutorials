e74843f5ab5cc55e79c6b6e0368e9674ea394e78
Anuj Rajput <anujrajput04@gmail.com>
2020-10-19 14:06:50 +0530

    App Settings

---
+++ Projects/RabbleWabble/RabbleWabble/Models/AppSettings.swift
@@ -0,0 +1,64 @@
+//
+//  AppSettings.swift
+//  RabbleWabble
+//
+//  Created by Anuj Rajput on 14/10/20.
+//
+
+import Foundation
+
+public class AppSettings {
+    private struct Keys {
+        static let questionStrategy = "questionStrategy"
+    }
+    
+    // MARK: - Instance Properties
+    private let userDefaults = UserDefaults.standard
+    
+    public var questionStrategyType: QuestionStrategyType {
+        get {
+            let rawValue = userDefaults.integer(forKey: Keys.questionStrategy)
+            return QuestionStrategyType(rawValue: rawValue)!
+        }
+        set {
+            userDefaults.setValue(newValue.rawValue, forKey: Keys.questionStrategy)
+        }
+    }
+    
+    // MARK: - Static Properties
+    public static let shared = AppSettings()
+    
+    // MARK: - Object Lifecycle
+    private init() { }
+    
+    // MARK: - Instance Methods
+    public func questionStrategy(for questionGroup: QuestionGroup) -> QuestionStrategy {
+        return questionStrategyType.questionStrategy(for: questionGroup)
+    }
+}
+
+// MARK: - QuestionStrategyType
+public enum QuestionStrategyType: Int, CaseIterable {
+    
+    case random
+    case sequential
+    
+    // MARK: - Instance Method
+    public func title() -> String {
+        switch self {
+        case .random:
+            return "Random"
+        case .sequential:
+            return "Sequential"
+        }
+    }
+    
+    public func questionStrategy(for questionGroup: QuestionGroup) -> QuestionStrategy {
+        switch self {
+        case .random:
+            return RandomQuestionStrategy(questionGroup: questionGroup)
+        case .sequential:
+            return SequentialQuestionStrategy(questionGroup: questionGroup)
+        }
+    }
+}
